13f6f46e0ba63861164707d15d9bd5f43ed88ee6
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2020-09-20 21:50:08 -0500

    added write file feature to modify JSON file with user input

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,13 +1,8 @@
 const fs = require('fs')
-
 const path = require('path')
-
 const notesDb = require('../db/db.json');
-
 const dbPath = path.join(__dirname, "../db/db.json")
 
-// console.log(typeof (notesDb))
-// console.log(notesDb)
 
 module.exports = function (app) {
 
@@ -18,33 +13,17 @@ module.exports = function (app) {
 
 
     app.post("/api/notes", function (req, res) {
-
-
         req.body.id = notesDb.length
-
         notesDb.push(req.body)
         console.log(notesDb)
 
         const notesJSON = JSON.stringify(notesDb)
 
-        // fs.writeFile(dbPath, notesJSON, (err) => {
-        //     if (err) throw err;
-        //     console.log('the file has been saved!')
-        // })
-
-        // .then(() => {
-        //     fs.readFileSync(dbPath, 'utf8', function (err, data) {
-        //         if (err) throw err
-        //         console.log(data)
-        //         res.json(data)
-
-        //     })
-        // })
+        fs.writeFile(dbPath, notesJSON, (err) => {
+            if (err) throw err;
+            console.log('the file has been saved!')
+        })
         res.json(true)
-
-
-
-
     });
 
 
