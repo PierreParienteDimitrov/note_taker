815409fe42f288d358f9cab21a595d8d0dcd7233
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2020-09-20 23:42:51 -0500

    created app.delete in apiRoute

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
-const path = require('path')
-const notesDb = require('../db/db.json');
+const path = require('path');
+let notesDb = require('../db/db.json');
 const dbPath = path.join(__dirname, "../db/db.json")
 
 
@@ -17,14 +17,42 @@ module.exports = function (app) {
         notesDb.push(req.body)
         console.log(notesDb)
 
-        const notesJSON = JSON.stringify(notesDb)
-
-        fs.writeFile(dbPath, notesJSON, (err) => {
-            if (err) throw err;
-            console.log('the file has been saved!')
-        })
+        writeFile(notesDb)
         res.json(true)
     });
 
+    app.get("/api/notes/:id", function (req, res) {
+        let noteId = req.params.id
+        const filteredNotes = notesDb.filter((obj) => obj.id == noteId)
+        if (filteredNotes.length > 0) {
+            res.json(filteredNotes)
+        } else {
+            res.send("not found")
+        }
+    })
 
+    app.delete("/api/notes/:id", function (req, res) {
+        let noteId = parseInt(req.params.id)
+        console.log(typeof (noteId))
+
+        // console.log(notesDb[noteId])
+
+        console.log("Note ID is " + noteId)
+
+        notesDb = notesDb.filter((obj) => obj.id != noteId)
+
+        console.log("---------")
+        console.log(notesDb)
+        writeFile(notesDb)
+        res.json(true)
+    })
+}
+
+function writeFile(data) {
+    const notesJSON = JSON.stringify(data)
+
+    fs.writeFile(dbPath, notesJSON, (err) => {
+        if (err) throw err;
+        console.log('the file has been saved!')
+    })
 }
